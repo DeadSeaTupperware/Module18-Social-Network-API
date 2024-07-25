caa6ac26ab31aeff0837dfb758a9c553204de607
Dean Bloomingdale <dean.bloomingdale1@gmail.com>
2024-07-25 15:03:03 -0400

    create/delete reaction functionality

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -67,4 +67,32 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
+
+  createReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $addToSet: { reactions: req.body } },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought found with that ID." })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  deleteReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $pull: { reactions: { reactionId: req.params.reactionId } } },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought found with that ID." })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 };
