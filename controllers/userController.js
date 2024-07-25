ee9008a565980df246f3db728b9b7b406749a433
Dean Bloomingdale <dean.bloomingdale1@gmail.com>
2024-07-25 17:35:04 -0400

    fixed typo in deleteFriend route

--- controllers/userController.js
+++ controllers/userController.js
@@ -75,8 +75,7 @@ module.exports = {
       .then((user) => {
         !user
           ? res.status(404).json({ message: "No User found with that ID." })
-          : res,
-          json(user);
+          : res.json(user);
       })
       .catch((err) => res.status(500).json(err));
   },
